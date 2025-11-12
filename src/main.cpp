17f857c973f4ac657880690e7707bd759005b158
Mikoangelo20 <migangahuga@gmail.com>
2025-11-11 18:00:13 -0600

    Change Filepath to work on mac and windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include "Audio.h"
 #include <vector>
 
+#ifdef _WIN32
+  #define INPATH "../rsc/SoundFile_01.wav"
+  #define OUTPATH  "../rsc/out2.wav"
+#endif
+
+#ifdef __APPLE__
+  #define INPATH "../../rsc/SoundFile_01.wav"
+  #define OUTPATH "../../rsc/out2.wav"
+#endif
+
+#ifdef __UNIX__
+  #define INPATH "../../rsc/SoundFile_01.wav"
+  #define OUTPATH "../../rsc/out2.wav"
+#endif
+
 
 int main()
 {
   Audio audio;
 
-  //audio.decode("../rsc/SoundFile_01.wav");
-  audio.decode("../rsc/SoundFile_01.wav");
+  audio.decode(INPATH);
 
   audio.processAudio();
 
-  audio.encode("../rsc/out2.wav");
+  audio.encode(OUTPATH);
 
   
 
